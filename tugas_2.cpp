4d14b62c11f461d8944af13e62ded3b27b3ed5c8
Nosa Rahmanda <162148196+Ifwtzzzzzh@users.noreply.github.com>
2024-04-26 17:01:37 +0700

    Update tugas_2.cpp

--- tugas_2.cpp
+++ tugas_2.cpp
@@ -1,72 +1,108 @@
 #include <iostream>
 using namespace std;
 
+double rectangleWide(double a, double b) {
+    double result = a * b;
+    cout << "Hasil dari Luas Segi Empat Adalah: " << result << endl;
+    return result;
+}
+
+double triangleWide(double a, double b) {
+    double result = a * b / 2;
+    cout << "Hasil dari Luas Segitiga Adalah: " << result << endl;
+    return result;
+}
+
 int hitKeliling(int a, int b) {
     int result = a+b;
-    cout << "Keliling : " << result;
+    cout << "Keliling : " << result << endl;
     return result;
 }
 
 double hitKeliling(double a, double b) {
     double result = a+b;
-    cout << "Keliling : " << result;
+    cout << "Keliling : " << result << endl;
     return result;
 }
 
 float hitKeliling(float a, float b, float c) {
     float result = a+b+c;
-    cout << "Keliling : " << result;
+    cout << "Keliling : " << result << endl;
     return result;
 }
 
+void showDesc(string desc) {
+    cout << "Masukan Pilihan" << endl;
+    cout << "1. Hitung " << desc << " Persegi" << endl;
+    cout << "2. Hitung " << desc << " Persegi Panjang" << endl;
+    cout << "3. Hitung " << desc << " Segitiga" << endl;
+}
+
 int main() {
-    int a, b, operation;
+    int a, b, operation, operation2;
     double c, d;
     float e, f, g;
-    cout << "Menghitung Keliling Segitiga, Persegi, dan Persegi Panjang" << endl;
-    cout << "=====================================" << endl;
-    cout << "1. Keliling Segitiga" << endl;
-    cout << "2. Keliling Persegi" << endl;
-    cout << "3. Keliling Persegi Panjang" << endl;
-    cout << "0. Keluar" << endl;
-    cout << "=====================================" << endl;
+    string desc, option;
 
-    cout << "Pilih operasi: ";
-    cin >> operation;
-    if (operation == 1) {
-        cout << "Masukkan bilangan alas: ";
-        cin >> e;
-        cout << "Masukkan bilangan tinggi: ";
-        cin >> f;
-        cout << "Masukkan bilangan sisi miring: ";
-        cin >> g;
-    } else if (operation == 2) {
-        cout << "Masukkan bilangan panjang: ";
-        cin >> a;
-        cout << "Masukkan bilangan lebar: ";
-        cin >> b;
-    } else if (operation == 3) {
-        cout << "Masukkan bilangan panjang: ";
-        cin >> c;
-        cout << "Masukkan bilangan lebar: ";
-        cin >> d;
-    } else if (operation == 0) {
-        cout << "Program selesai";
-    }
 
-    switch (operation) {
+    do {
+        cout << "Masukan Pilihan" << endl;
+        cout << "1. Hitung Luas" << endl;
+        cout << "2. Hitung Keliling" << endl;
+        cout << "Pilihan: ";
+        cin >> operation;
+
+        switch (operation) {
         case 1:
-        hitKeliling(e,f,g);
-        break;
+            desc = "Luas";
+            showDesc(desc);
+            break;
         case 2:
-        hitKeliling(a,b);
-        break;
-        case 3:
-        hitKeliling(c,d);
-        break;
+            desc = "Keliling";
+            showDesc(desc);
+            break;
         default:
-        cout << "Program Tidak ada";
-        break;
-    }
+            break;
+        }
+
+        cout << "Pilihan: ";
+        cin >> operation2;
+        if (operation == 1 || operation2  == 2) {
+            cout << "Masukkan bilangan sisi 1: ";
+            cin >> a;
+            cout << "Masukkan bilangan sisi 2: ";
+            cin >> b;
+        } else if (operation == 2 && operation2 == 2) {
+            cout << "Masukkan bilangan panjang: ";
+            cin >> c;
+            cout << "Masukkan bilangan lebar: ";
+            cin >> d;
+        } else if (operation == 2 && operation2 == 3) {
+            cout << "Masukkan bilangan sisi 1: ";
+            cin >> e;
+            cout << "Masukkan bilangan sisi 2: ";
+            cin >> f;
+            cout << "Masukkan bilangan sisi 3: ";
+            cin >> g;
+        } else {
+            cout << "Pilihan Tidak ada";
+        }
+
+        if (operation == 1 && operation2 <= 2) {
+            rectangleWide(a,b);
+        } else if (operation == 1 && operation2 == 3) {
+            triangleWide(a,b);
+        } else if (operation == 2 && operation2 == 1) {
+            hitKeliling(a,b);
+        } else if (operation == 2 && operation2 == 2) {
+            hitKeliling(c,d);
+        } else if (operation == 2 && operation2 == 3) {
+            hitKeliling(e,f,g);
+        }
+        cout << "Ulangi y/n? ";
+        cin >> option;
+        // system("cls");
+    } while (option == "y" || option == "Y");
+    cout << "Program Selesai";
     return 0;
 }
